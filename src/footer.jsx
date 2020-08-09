b7592f3fd0c75749f5cdcb1bc51c64c52c52fb86
Benoit Arnoult <benar77@gmail.com>
2020-08-09 14:27:22 +0200

    Created Footer.jsx

---
+++ src/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  './MarkdownEditor.css'
+class Footer extends React.Component {
+    render() {
+        return(
+            <p>Created by Benoît Arnoult <a href="">Github</a>&nbsp;<a href="https://www.benoit-arnoult.xyz">Website</a></p>
+
+
+        )
+    }
+}
+
+export default Footer;
