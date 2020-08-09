7355d1eb549ba21f1d78e38b2b6f399fa0b63078
Benoit Arnoult <benar77@gmail.com>
2020-08-09 14:31:04 +0200

    footer styling

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,7 +3,7 @@ import  './MarkdownEditor.css'
 class Footer extends React.Component {
     render() {
         return(
-            <p>Created by Benoît Arnoult <a href="https://github.com/PiiXelx64/">Github</a>&nbsp;<a href="https://www.benoit-arnoult.xyz">Website</a></p>
+            <p class="footer">Created by Benoît Arnoult <a href="https://github.com/PiiXelx64/">Github</a>&nbsp;<a href="https://www.benoit-arnoult.xyz">Website</a></p>
 
 
         )
