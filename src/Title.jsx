70645c0fc927d2e748f5758ae2e9e6d5149485df
Benoit Arnoult <benar77@gmail.com>
2020-08-09 15:00:49 +0200

    Simplified Title

--- src/Title.jsx
+++ src/Title.jsx
@@ -5,12 +5,6 @@ class Title extends React.Component {
         return(
             <div className="Title">
                 <h2>Simple markdown editor.</h2>
-                <p>Usage :</p>
-                <ul>
-                    <li>Write markdown in the text area</li>
-                    <li>See the result</li>
-                    <li>Click the "Copy to clipboard" button to copy the markdown you wrote.</li>
-                </ul>
             </div>
 
 
